521cc37cde9c0acd267195ffa9c8592fe21e0318
Robério <rroberiosouza@gmail.com>
2021-04-21 23:42:44 -0300

    Criado o construtor. Colocado os metodos get, set e initialize.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,61 @@
 class CalcController {
 
+    constructor(){
+        this._locale = 'pt-BR';
+        this._displayEL = document.querySelector('#display');
+        this._dateEL = document.querySelector('#data');
+        this._timeEL = document.querySelector('#hora');
 
-    
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        this.displayDateTime();
+        setInterval(() => {
+            this.displayDateTime();
+        }, 1000);
+        
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+    }
+
+    displayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime(){
+        return this._timeEL.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEL.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEL.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEL.innerHTML = value;
+    }
+
+    get display(){
+        return this._displayEL.innerHTML;
+    }
+
+    set display(value){
+        this._displayEL.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
 }
