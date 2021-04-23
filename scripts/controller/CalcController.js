cacca8ea5e156fbffccc5e41ff02a9b8fc734a98
Roberio Souza <rroberiosouza@outlook.com.br>
2021-04-22 21:44:53 -0300

    Colocado o método initButtonsEvents no construtor. Criado o método addEventListenerAll que executa vários eventos de uma unica vez.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,6 +7,7 @@ class CalcController {
         this._timeEL = document.querySelector('#hora');
 
         this._currentDate;
+        this.initButtonsEvents();
         this.initialize();
     }
 
@@ -20,6 +21,12 @@ class CalcController {
 
     initButtonsEvents(){
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index)=>{
+            btn.addEventListener('click', e=>{
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+        })
     }
 
     displayDateTime(){
